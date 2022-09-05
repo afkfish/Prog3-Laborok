b5fd0f6122b67220ff7e6c8c55f1344f02470797
afkfish <makisbeni2003@gmail.com>
2022-09-05 23:01:39 +0200

    Lab1 IMSC feladat

--- Lab1/masodik_feladat/sample/calc/Calculator.java
+++ Lab1/masodik_feladat/sample/calc/Calculator.java
@@ -1,7 +1,14 @@
 package sample.calc;
 
 public class Calculator{
+	private int sum;
+	private void store(int a) {
+		sum += a;
+		System.out.println(sum);
+	}
+
 	public int add(int a, int b) {
+		store(a);
 		return a+b;
 	}
 }
